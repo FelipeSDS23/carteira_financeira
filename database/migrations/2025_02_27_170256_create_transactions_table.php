bcc774ae8cb4870334e840212a31170fd724d0de
FelipeSDS23 <felipe03san@gmail.com>
2025-02-27 17:26:26 -0300

    Criadas migrations, controllers, models e rotas de transactions e accounts

--- database/migrations/2025_02_27_170256_create_transactions_table.php
+++ database/migrations/2025_02_27_170256_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('account_id')->constrained()->onDelete('cascade'); // Chave estrangeira para a tabela accounts / Conta de origem
-            // $table->foreignId('origin_account_id')->nullable()->constrained('accounts')->onDelete('set null'); // Conta de origem 
+            // $table->foreignId('origin_account_id')->nullable()->constrained('accounts')->onDelete('set null'); // Conta de origem
             $table->foreignId('destination_account_id')->nullable()->constrained('accounts')->onDelete('set null'); // Conta de destino
             $table->decimal('amount', 11, 2); // Valor da transação
             $table->enum('type', ['deposit', 'transfer']); // Tipo da transação (depósito ou transferência)
